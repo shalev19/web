284c72a5c559517810dcbf912110bd2181604c7a
Shalev Yohanan <61795827+shalev19@users.noreply.github.com>
2022-01-02 11:08:46 +0200

    Add files via upload

---
+++ hw3/realm-tutorial-web/build/precache-manifest.088831776a2b94ae53b7f4289bc75d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce09ba2681c1378bb5b0e6256be2affc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e60d1c4e1a819ab57923",
+    "url": "/static/css/main.c11ef4eb.chunk.css"
+  },
+  {
+    "revision": "2609521c893c946ea60a",
+    "url": "/static/js/2.c659f025.chunk.js"
+  },
+  {
+    "revision": "ec19a7404159eb2ec065d16f9bab6f00",
+    "url": "/static/js/2.c659f025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60d1c4e1a819ab57923",
+    "url": "/static/js/main.cace4971.chunk.js"
+  },
+  {
+    "revision": "8d8e7605b29cbcd435f9",
+    "url": "/static/js/runtime-main.62776961.js"
+  }
+]);
